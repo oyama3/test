c2e46d67f6064fb8fd1350928c4feff0077a058c
oyama <t.oyama@dverse.me>
2016-09-13 18:53:32 +0900

    ademo

---
+++ Assets/AutoRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0.05f,0));
+    }
+}
---
+++ Assets/cont.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cont : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //左スティック（今までと同じ）
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            //上に傾いている
+            Debug.Log("ue!");
+        }
+        else if (0 < Input.GetAxisRaw("Vertical"))
+        {
+            //下に傾いている
+            Debug.Log("Shita!");
+        }
+        else
+        {
+            //上下方向には傾いていない
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            //左に傾いている
+            Debug.Log("Left");
+        }
+        else if (0 < Input.GetAxisRaw("Horizontal"))
+        {
+            //右に傾いている
+            Debug.Log("Right!");
+        }
+        else
+        {
+            //左右方向には傾いていない
+        }
+
+
+    }
+}
